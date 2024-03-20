17391f3b834d48b53856f10493af2311ea0bbde9
CarlosMany <100442564+CarlosMany@users.noreply.github.com>
2024-03-20 15:27:05 -0600

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ window.addEventListener("scroll", function () {
   var infoDiv = document.querySelector(".informacion1");
   var scrollPosition = window.scrollY;
 
-  // Suponiendo que quieras que el efecto comience después de 300px de scroll
-  if (scrollPosition > 150) {
-    var scale = 1 - (scrollPosition - 150) / 1000; // Ejemplo de cálculo para el efecto de escala
+  // El efecto comience después de 300px de scroll
+  if (scrollPosition > 148) {
+    var scale = 1 - (scrollPosition - 148) / 1000; // Ejemplo de cálculo para el efecto de escala
     scale = Math.max(scale, 0); // Evitar escala negativa
     video.style.transform = "scale(" + scale + ")";
 
